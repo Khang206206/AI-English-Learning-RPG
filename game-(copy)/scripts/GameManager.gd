f34c081ca8cf9031e15e2a7cda11b20e4ac3166d
duongthienphu <24122047@student.hcmus.edu.vn>
2026-05-24 00:25:36 +0700

    Xóa folder thừa

--- game-(copy)/scripts/GameManager.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-# Lưu tên của cổng đích mà nhân vật sẽ xuất hiện tại đó
-var target_spawn_id = ""
--- game-(copy)/scripts/instructor_side_view.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-@onready var sprite = $AnimatedSprite2D 
-
-func _ready():
-	# Luôn chơi hoạt ảnh đứng yên khi bắt đầu
-	if sprite.sprite_frames.has_animation("idle"):
-		sprite.play("idle") 
-	
